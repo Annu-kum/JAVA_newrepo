28db8d941efa4420eaf3667fdef7c42a1f735af0
Annu-kum <annuk5523@gmail.com>
2024-12-27 15:44:18 +0530

    added new code

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
             System.out.println("success");
             System.out.println("failed");
             System.out.println("successFailed");
+            System.out.println("formate");
 
         }
     }
