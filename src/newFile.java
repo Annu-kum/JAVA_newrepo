338dae370646c6cc86373683af24aeed553886c1
Annu Kumari <123096055+Annu-kum@users.noreply.github.com>
2024-12-27 10:36:46 +0530

    Revert "add new file"

--- src/newFile.java
+++
@@ -1,9 +0,0 @@
-public class newFile {
-
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-}
-}
